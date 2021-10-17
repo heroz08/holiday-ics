3bb2102309d5a80d4fb43975e9a3c750bc404ddc
hzhyang <zilongerdai@gmail.com>
2021-10-17 21:43:48 +0800

    爬虫获取假日安排

---
+++ getHolidayInfo.js
@@ -0,0 +1,87 @@
+const fs = require('fs')
+const cheerio = require('cheerio')
+const axios = require('axios')
+const moment = require('moment');
+
+async function getHolidayInfo(year) {
+    this.year = year
+    const href = await search()
+    const holidayInfo = await getPageInfoByUrl(href)
+    return holidayInfo
+}
+
+async function search() {
+    const searchUrl = 'http://sousuo.gov.cn/s.htm';
+    const params = {
+        title: this.year,
+        t: 'paper',
+        advance: true,
+        q: "假期",
+        pcodeJiguan: "国办发明电",
+        puborg: "国务院办公厅",
+
+    }
+    const resp = await axios.get(searchUrl, {params})
+    const $ = cheerio.load(resp.data)
+    const href = cheerio.load($('.result .res-list')[0])(' h3 a').attr('href')
+    return href
+}
+
+async function getPageInfoByUrl(href) {
+    const condition = ['一、', '二、', '三、', '四、', '五、', '六、', '七、'];
+    const filter = []
+    const resp = await axios.get(href)
+    const $ = cheerio.load(resp.data)
+    const infoWrap = $('.b12c p')
+    for (let i = 0; i < infoWrap.length; i++) {
+        const p = infoWrap[i]
+        const c = cheerio.load(p)
+        condition.forEach(co => {
+            const text = c.text()
+            if (text.includes(co)) {
+                filter.push(text)
+            }
+        })
+    }
+    return dealTextInfo(filter)
+}
+
+function dealTextInfo(arr) {
+    const info = []
+    const deReg = /[\u4e00-\u9fa5]{2,3}：/
+    const startReg = /(\d{1,2}[\u4e00-\u9fa5]{1}\d{1,2}[\u4e00-\u9fa5]{1}至|\d{1,2}[\u4e00-\u9fa5]{1}\d{1,2}[\u4e00-\u9fa5]{1}放)/
+    const endReg = /至\d{1,2}[\u4e00-\u9fa5]{1}放假/
+    const monthReg = /^\d{1,2}-/
+    // const restReg = /调休/
+    const workReg = /\d{1,2}[\u4e00-\u9fa5]{1}\d{1,2}[\u4e00-\u9fa5]{1}（[\u4e00-\u9fa5]{3}）/g
+    arr.forEach(text => {
+        const obj = {}
+        obj.name = text.match(deReg)?.[0].slice(0, -1)
+        obj.start = text.match(startReg)?.[0].slice(0, -1).replace(/(日|月)/g, '-').slice(0,-1)
+        const month = obj.start.match(monthReg)?.[0]
+        const endDate = text.match(endReg)
+        if (endDate) {
+            obj.end = (month + endDate[0].slice(1, -2)).replace(/(日|月)/g, '-').slice(0,-1)
+        }
+        const workDays = text.match(workReg)
+        obj.workDays = workDays ? workDays.map(day => (day.slice(0, -5)).replace(/(日|月)/g, '-').slice(0,-1)) : []
+        obj.desc = text.replace(/[\u4e00-\u9fa5]{1}、[\u4e00-\u9fa5]{2,3}：/, '')
+        info.push(obj)
+    })
+    createJson(info)
+    return info
+}
+
+
+function createJson(arr) {
+    fs.appendFile(`./json/${this.year}.json`, JSON.stringify(arr), (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    })
+}
+
+// getHolidayInfo(2021)
+
+module.exports = getHolidayInfo
---
+++ index.js
@@ -0,0 +1,154 @@
+const ics = require('ics');
+const fs = require('fs');
+
+const moment = require('moment');
+const axios = require('axios');
+const iconv = require('iconv-lite');
+const getHolidayInfo = require('./getHolidayInfo');
+
+const currentYear = 2021;
+
+
+function createIcs(events) {
+  const {errors, value}  = ics.createEvents(events);
+  if (errors) {
+    console.log(errors);
+    return;
+  }
+  // console.log(value);
+  fs.appendFile('event.ics',  value, (err) => {
+    if (err){
+      console.log(err);
+      return ;
+    }
+  })
+}
+
+function  createEvents(allHolidayInfo) {
+  const events = [];
+  allHolidayInfo.forEach(obj => {
+    if (obj.isholiday || obj.isWork) {
+
+      const startDate = obj.date.split('-');
+      const endDate = moment(obj.date).add(1,  'day').format('YYYY-M-D').split('-');
+
+      const event = {
+        start: startDate,
+        end: endDate,
+        title: obj.name,
+        status: 'CONFIRMED',
+        productId: 'hzy@hzhyang.com',
+        description: (obj.type ? obj.type : '') + (obj.desc ? ' ' +obj.desc : ''),
+      }
+      events.push(event);
+    }
+  });
+  // console.log(events)
+  return events;
+}
+
+
+
+function getAllDayinYear () {
+  const startDay = moment().year(currentYear).startOf('year');
+  const endDay = moment().year(currentYear + 1).startOf('year');
+  const diffDay = endDay.diff(startDay, 'day');
+
+  const allDayInCurrentYearArr = [];
+
+  for (i = 0; i < diffDay; i++) {
+    const obj = {};
+    const _startDay = moment().year(currentYear).startOf('year');
+    obj.date = _startDay.add(i, 'day').format('YYYY-M-D');
+    allDayInCurrentYearArr.push(obj);
+  }
+  return allDayInCurrentYearArr;
+}
+
+
+function read () {
+  const read = new Promise((resolve, reject) => {
+    fs.readFile(`./json/${currentYear}.json`, 'utf8',(err, data) => {
+      if(err) reject(err)
+      resolve(JSON.parse(data))
+    })
+  })
+  return read
+}
+
+function exist () {
+  const exist = new Promise((resolve, reject) => {
+    fs.stat(`./json/${currentYear}.json`, (err, stats) => {
+      resolve(stats)
+    })
+  })
+  return exist
+}
+
+
+async function getHoliday (allDaysInYear) {
+  let holiday = []
+  const stats = await exist()
+  if(!stats) {
+    holiday = await getHolidayInfo(currentYear)
+  } else {
+    holiday = await read()
+  }
+  holiday.forEach(item => {
+    const start = moment(`${currentYear}-${item.start}`)
+    if(item.end) {
+      const end = moment(`${currentYear}-${item.end}`)
+      allDaysInYear.forEach(_item => { // 放假日子
+        if(moment(_item.date).isBetween(start, end, 'day', [])) {
+          _item.isholiday = true;
+          _item.name = item.name;
+          _item.type = '休';
+          _item.desc = item.desc
+        }
+      })
+    } else {
+      allDaysInYear.forEach(_item => { // 放假日子
+        if(_item.date === start) {
+          _item.isholiday = true;
+          _item.name = item.name;
+          _item.type = '休';
+          _item.desc = item.desc
+        }
+      })
+    }
+    // work
+    if(item.workDays.length) {
+      item.workDays.forEach(workDate => {
+        const index = getIndexInArr(allDaysInYear, `${currentYear}-${workDate}`);
+        if (index !== -1) {
+          const _item = allDaysInYear[index];
+          _item.name = '上班--' + item.name + '--补班';
+          _item.isWork = true;
+          _item.type = '班';
+          _item.desc = item.desc
+        }
+      })
+    }
+  })
+  return allDaysInYear;
+}
+
+function getIndexInArr (arr, item) {
+  const len = arr.length;
+ for (i = 0; i <  len; i++) {
+   if (arr[i].date === item) {
+     return i;
+   }
+ }
+ return -1;
+}
+
+
+async function start() {
+  const allDayInCurrentYearArr = getAllDayinYear();
+  const allHolidayInfo = await getHoliday(allDayInCurrentYearArr);
+  const events = createEvents(allHolidayInfo);
+  createIcs(events);
+}
+
+start();
