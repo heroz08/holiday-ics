3900508012957042876ebc5f8e1931b4f01c26bb
heroz08 <zilongerdai@gmail.com>
2023-01-02 13:40:01 +0800

    3

--- src/utils/getHolidayInfo.js
+++ src/utils/getHolidayInfo.js
@@ -94,6 +94,7 @@ function dealTextInfo(arr) {
 
 function createJson(arr) {
     const tagPath = path.resolve(__dirname, `../../json/${this.year}.json`)
+    console.log(tagPath,'path', path.join(__dirname,`../../json/${this.year}.json`));
     fs.appendFile(tagPath, JSON.stringify(arr), (err) => {
         if (err) {
             console.log(__dirname,'dirname');
