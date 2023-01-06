6eac586fc4c4e6413c14996b518f5da404c983e5
zz <zilongerdai@gmail.com>
2023-01-06 15:09:28 +0800

    去掉src层

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line node/no-unpublished-require
-const start = require('./build/src/app');
+const start = require('./build/app');
 console.log('start');
 async function main() {
   const {status, error} = await start();
