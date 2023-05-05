82d7582c0bac07e3adba51e77e89d1e35d8f9832
heroz08 <zilongerdai@gmail.com>
2023-05-05 22:45:21 +0800

    添加判断是否为假期的服务接口

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // eslint-disable-next-line node/no-unpublished-require
 const start = require('./build/app');
+const startServer = require('./app');
 console.log('start');
 async function main() {
   const {status, error} = await start();
   if (!status) {
     console.log(error);
   }
+  startServer();
 }
 main();
