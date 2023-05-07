21b271eac4cbf25bd6fdc69be353b5e634516ffb
heroz08 <zilongerdai@gmail.com>
2023-05-07 14:11:46 +0800

    测试request和response

--- api/holiday.js
+++ api/holiday.js
@@ -3,26 +3,26 @@ const fs = require('fs/promises');
 const path = require('path');
 const moment = require('moment');
 
-export default async function holiday(request, response) {
-  response.json({
-    query: request.query,
-    body: request.body,
-    res: request,
-    resp: response
-  })
+export default function holiday(request, response) {
   try {
     console.log(request.query, request.body, request);
-    const { day } = request.body;
-    const [y] = day.split('-');
-    const jsonData = await fs.readFile(path.resolve(__dirname, '../public/json/' + y + '.json'));
-    const isHoliday = JSON.parse(jsonData.toString()).some(item => {
-      const { startYear, endYear, start, end } = item;
-      const startDay = (startYear || y) + '-' + start;
-      const endDay = (endYear || y) + '-' + (end || start);
-      const momentDay = moment(day);
-      return momentDay.isBetween(startDay, endDay, 'day', '[]');
-    });
-    response.json({ isHoliday, success: true, message: '成功' });
+    response.json({
+      query: request.query,
+      body: request.body,
+      res: request,
+      resp: response
+    })
+    // const { day } = request.body;
+    // const [y] = day.split('-');
+    // const jsonData = await fs.readFile(path.resolve(__dirname, '../public/json/' + y + '.json'));
+    // const isHoliday = JSON.parse(jsonData.toString()).some(item => {
+    //   const { startYear, endYear, start, end } = item;
+    //   const startDay = (startYear || y) + '-' + start;
+    //   const endDay = (endYear || y) + '-' + (end || start);
+    //   const momentDay = moment(day);
+    //   return momentDay.isBetween(startDay, endDay, 'day', '[]');
+    // });
+    // response.json({ isHoliday, success: true, message: '成功' });
   } catch (e) {
     response.json({ success: false, message: e });
   }
